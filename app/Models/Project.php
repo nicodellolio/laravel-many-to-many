c1800c47613dae2ec9ff871b328cd36413215fbc
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-27 16:10:27 +0200

    add project_technology table
    compile the models relationship and foreign keys

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Type;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -13,10 +14,23 @@ class Project extends Model
 
     protected $fillable = ['title', 'slug', 'description', 'type_id', 'project_start_date', 'project_end_date', 'link_to_source_code', 'link_to_project_view', 'preview_image'];
 
-    
+    /**
+     * The types that belong to the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
     }
-}
 
+    /**
+     * The technologies that belong to the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Technology extends Model
 {
     use HasFactory;
+
+    protected $fillable = [];
+
+    /**
+     * The projects that belong to the Technology
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
+    }
+
 }
